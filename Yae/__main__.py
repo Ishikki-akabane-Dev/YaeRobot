d3c4f96dbaad4e64ee86413b01813ab722d6ca13
Ishikki-akabane-Dev <106568950+Ishikki-akabane-Dev@users.noreply.github.com>
2022-06-07 11:03:20 +0530

    Update __main__.py

--- Yae/__main__.py
+++ Yae/__main__.py
@@ -230,7 +230,6 @@ def Yae_about_callback(update: Update, context: CallbackContext):
             \n ‣ I can Warn Users until they reach max Warns, with each predefined actions such as Ban, Mute and Kick etc.
             \n ‣ I have Note Keeping System, Blacklists, And even Predetermined replies on certain keywords.
             \n ‣ I check Admins Permissions before perform any Command and more Stuffs.
-            \n ‣ I have an advanced Artificial Chatbot System, so can talk with users like humans.
             \n\n*If you have any Question, You can join Support Chat. My Developer Team will Answer. Check Link Below*""".format(
                         escape_markdown(first_name), escape_markdown(context.bot.first_name)),
             parse_mode=ParseMode.MARKDOWN,
